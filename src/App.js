088fbbc5389589d4a58db0a25763a71a589271f7
Mrityunjay Kumar <mk10399@gmail.com>
2023-12-26 12:50:31 +0530

    Weather-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const API_KEY = 'b9e675496db047ffbfa62858232612';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [weatherData, setWeatherData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const callApi = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${input}`);
+      const data = await response.json();
+      console.log(data);
+      let weatherData = [data.current.temp_c, data.current.humidity, data.current.condition.text, data.current.wind_kph];
+      setWeatherData(weatherData);
+      setLoading(false);
+    } catch (error) {
+      alert('Failed to fetch weather data');
+      setLoading(false);
+      setWeatherData([]);
+      return null;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <input type="text" value={input} onChange={handleInput} placeholder='Enter city name'/>
+        <button onClick={callApi}>Search</button>
+        {weatherData.length >0 && <div className='card-container'>
+          <div className='card'>
+            <h5>Temperature</h5>
+            <div>{weatherData[0]} C </div>
+          </div>
+          <div className='card'>
+            <h5>Humidity</h5>
+            <div>{weatherData[1]}%</div>
+          </div>
+          <div className='card'>
+            <h5>Condition</h5>
+            <div>{weatherData[2]} </div>
+          </div>
+          <div className='card'>
+            <h5>Wind Speed</h5>
+            <div>{weatherData[3]} kph</div>
+          </div>
+        </div>}
+        {loading && <div>Loading data...</div>}
     </div>
   );
 }
